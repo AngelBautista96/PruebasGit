850b47bd2198f74a00a92f3b584d96688747bf4a
SOPORTE TECNICO <SOPORTE_IT@odefhn.onmicrosoft.com>
2020-07-07 21:47:08 -0600

    Agregar archivos de proyecto.

---
+++ PruebasGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebasGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
